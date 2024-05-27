1173189dd01d704ae9b6840e52aadeeaf938a364
황유빈 <97329194+ppinppini@users.noreply.github.com>
2024-05-28 01:25:38 +0900

    5/28 01:25

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    },
+    server: {
+        proxy: {
+            // 문자열만 사용한다면 프록시를 거쳐 아래와 같이 변경된 주소로 요청된다.
+            "/survey": "http://43.200.172.87:8080", // 백엔드 api 주소
+        },
+    },
+});
+const getImage = async (hobbyId) => {
+    const data = await (await fetch(`http://43.200.172.87:8080`)).json();
+
+    return data;
+};
